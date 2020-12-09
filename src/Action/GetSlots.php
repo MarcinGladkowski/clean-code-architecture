b39ffb9682c4b2c6c03acc04946e009e0c4fbe65
Martin89PL <marcingladkowski@gmail.com>
2020-12-09 21:19:03 +0100

    ref: extract getSlots

---
+++ src/Action/GetSlots.php
@@ -0,0 +1,53 @@
+<?php declare(strict_types=1);
+
+namespace App\Action;
+
+use App\Controller\DoctorEntity;
+use App\Controller\SlotEntity;
+use App\Model\Doctors;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+class GetSlots
+{
+    /**
+     * @var Doctors
+     */
+    private Doctors $doctors;
+
+    public function __construct(Doctors $doctors)
+    {
+        $this->doctors = $doctors;
+    }
+
+    public function __invoke(Request $request, $doctorId)
+    {
+        $doctor = $this->doctors->getById((int)$doctorId);
+
+        if (!$doctor) {
+            return new JsonResponse([], 404);
+        }
+
+        $slots = $this->extractDoctorSlots($doctor);
+        return new JsonResponse(count($slots) ? $slots : []);
+    }
+
+    /**
+     * @param DoctorEntity $doctor
+     * @return SlotEntity[]
+     */
+    private function extractDoctorSlots(DoctorEntity $doctor): array
+    {
+        $slots = $doctor->slots();
+        $res = [];
+        foreach ($slots as $slot) {
+            $res[] = [
+                'id' => $slot->getId(),
+                'day' => $slot->getDay()->format('Y-m-d'),
+                'from_hour' => $slot->getFromHour(),
+                'duration' => $slot->getDuration()
+            ];
+        }
+        return $res;
+    }
+}
--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -3,14 +3,8 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use App\Controller\DoctorEntity;
-use DateTime;
-use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
 
 class Controller extends AbstractController
 {
@@ -18,57 +12,4 @@ class Controller extends AbstractController
     {
         return new JsonResponse('ReallyDirty API v1.0');
     }
-
-    /**
-     * @Route("/slots/{doctorId}", methods={"GET"})
-     * @param int $doctorId
-     * @param Request $request
-     * @return JsonResponse
-     */
-    public function getSlotsAction(int $doctorId, Request $request): ?JsonResponse
-    {
-        $doctor = $this->getDoctor($doctorId);
-
-        if ($doctor) {
-            /** @var SlotEntity[] $slots */
-            $slots = $this->extractDoctorSlots($doctor);
-            return new JsonResponse(count($slots) ? $slots : []);
-        }
-    }
-
-    /**
-     * @return ObjectManager|EntityManagerInterface
-     */
-    private function getObjectManager()
-    {
-        return $this->getDoctrine()->getManager();
-    }
-
-    private function getDoctor($doctorId)
-    {
-        return $this->getObjectManager()->createQueryBuilder()
-            ->select('doctor')
-            ->from(DoctorEntity::class, 'doctor')
-            ->where('doctor.id=:id')
-            ->setParameter('id', $doctorId)
-            ->setMaxResults(1)
-            ->getQuery()
-            ->getOneOrNullResult();
-    }
-
-    private function extractDoctorSlots(DoctorEntity $doctor): array
-    {
-        $slots = $doctor->slots();
-        $res = [];
-        foreach ($slots as $slot) {
-            $res[] = [
-                'id' => $slot->getId(),
-                'day' => $slot->getDay()->format('Y-m-d'),
-                'from_hour' => $slot->getFromHour(),
-                'duration' => $slot->getDuration()
-            ];
-        }
-        return $res;
-    }
-
 }
